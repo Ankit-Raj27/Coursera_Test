e6ea6c4e21b7bb4d07b3b3dbcc8b1991a525c6f2
Ankit Raj <ankitrajk27@gmail.com>
2022-04-18 17:10:30 +0530

    [update]

--- Module 4_solution/script.js
+++ Module 4_solution/script.js
@@ -1,5 +1,5 @@
-// ******************************* 
-// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
 //
 // Module 4 Assignment Instructions.
@@ -29,21 +29,21 @@ WARNING!!! WARNING!!!
 
 */
 
-// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
+// STEP 1:
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
-
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-// STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
+// STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
+// using the 'speak' method or either helloSpeaker's or byeSpeaker's
+// 'speak' method.
 // See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
 
-  // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
+  // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
   // names that start with either upper case or lower case 'J'/'j', call
@@ -52,7 +52,7 @@ for (var i = 0; i < names.length; i++) {
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt(0).toLowerCase();
 
-  // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
+  // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
